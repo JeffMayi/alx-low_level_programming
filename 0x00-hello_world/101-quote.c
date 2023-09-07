76c592fb6e41fc23259d075f58c9b75bf1e3a7ea
JeffMayi <geofrymayi@gmail.com>
2023-09-07 13:49:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+#include <unstd.h>
+/**
+ * main(A script that point a line to the standard error)
+ * Return 1(success)
+ */
+int main(void)
+	fput(and that piece of art is useful" - Dora Korpar, 2015-10-19\r,stdout);
+	Return(1);
