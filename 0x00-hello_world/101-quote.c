f74b1b8d7949c901cec934326755b5c446dc6ab5
JeffMayi <geofrymayi@gmail.com>
2023-09-09 12:17:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include  <unstd.h>
+#include <101-quote.c>
+#include <studio.h>
 /**
  * main -A script that point a line to the standard error
  * Return: always 0
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	Return: (1);
+fput("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
+Return:(1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+
 /**
  * main -A program that prints a line with puts function
  * Return: Always 0 (success)
+ *
  */
+
 int main(void)
+
 {
-  puts("\"Programming is like building a multilingual puzzle");
+
+puts(""Programming is like building a multilingual puzzle"\n"");
 Return:(0);
-}	
+
+}
