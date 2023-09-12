54a7066ecceb6f69f29c44a2cfa59d284ba50b09
JeffMayi <geofrymayi@gmail.com>
2023-09-12 10:50:43 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * main - program that prints _putchar , followed by new line
+ * Return: always 0
+ */
+int main(void)
+{
+	-putchar('_');
+	-putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+Return: (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - prototype of the main function
+ * Return: always 0
+ */
+{
+int _putchar(char C)
+print_alphabet();
+#endif
+}
