e0632bca8692c250f7f280d53e4f5dfa25657708
JeffMayi <geofrymayi@gmail.com>
2023-09-12 13:44:09 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - program that prints the alphabetic in loweercase
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z';letter++)
+	_putchar(letter0)
+	_putchar('\n');
+}
