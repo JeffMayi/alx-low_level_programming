e1a52533681343f790e6336cc8b1663d40255f2a
JeffMayi <geofrymayi@gmail.com>
2023-09-13 14:12:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,13 @@
-#ifndef main_h
-#define main_h
-/**
- * main - prototype of the main function
- * Return: always 0
- */
-{
-int _putchar(char);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(v0id);
-int_islower(int c);
-int_isalpa(int c);
+void print_alphabet_x10(void);
+int int_islower(int c);
+int int_isalpa(int c);
 int print_sign(int n);
-int_abs(int);
+int int_abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
@@ -19,4 +15,3 @@ int add(int,int);
 void print_to_98(int n);
 void print_times_table(int n);
 #endif
-}
