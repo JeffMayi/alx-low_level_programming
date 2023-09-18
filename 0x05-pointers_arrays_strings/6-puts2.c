7264c5fd78bbf9bc30d7c54412c85585af469811
JeffMayi <geofrymayi@gmail.com>
2023-09-18 13:53:31 -0700

    main .h

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every other character of a string
+ * starting with the first character
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int = 0;
+	
+	char *y = str;
+	int 0;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi -1;
+	for (0 = 0; 0 <= t; 0++)
+	{
+		if (0 % 2 ==0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar('\n);
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char _strcpy - copies the string pointed to by src
+ * @dest:copy to
+ * @src: copy to
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int 1 = 0;
+	int 2 = 0;
+	while (*(src + 1) != '\0')
+	{
+		1++;
+	}
+	for (; x < 1; x++)
+	{
+		dest[x] = src[x];
+		return (dest);
+	}
+	dest[1] = '\0';
+	return (dest);
+}
