240cec501e093da1cb7989c8573021f130f9e981
JeffMayi <geofrymayi@gmail.com>
2023-09-19 09:21:56 -0700

    x

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,24 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
-/** main - header files for More functions
- *  more nested loops
- *  projects
- *  Return : return 0
- */
+#include <stdio.h>
+#include <unistd.h>
 
-int _isupper(int c)
-int _isdigit(int c)
-int mul(int a, int b)
-void print_numbers(void)
-void print_most_numbers(void)
-void more_numbers(void)
-void print_line(int n)
-void print_diagonal(int n)
-void print_square(int size)
-void print_number(int n)
-void print_triangle(int size)
-void print_number(int n)
-void print_number(int n)
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_number(int n);
+void print_triangle(int size);
+void print_number(int n);
+void print_number(int n);
 
 #endif
 
