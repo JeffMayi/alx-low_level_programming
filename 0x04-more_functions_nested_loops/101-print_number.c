679f027b05efda6f37dd560a331e0fbcacdf41bb
JeffMayi <geofrymayi@gmail.com>
2023-09-23 01:03:03 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,46 @@
 #include "main.h"
+
 /**
- * print_number - that prints an integer
- * @n: interger to be printed
- * Return: void
+ * infinite_add - Add two positive numbers represented as strings
+ * @n1: The first number as a string
+ * @n2: The second number as a string
+ * @r: The buffer to store the result
+ * @size_r: The buffer size
+ *
+ * Return: Pointer to the result or 0 if result cannot be stored in r
  */
-void print_number(int n)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	_unsigned int n1;
-	if (n < 0)
+	int len1 = 0, len2 = 0, carry = 0;
+	int sum, index_r;
+	
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+	r[size_r - 1] = '\0';
+	index_r = size_r - 2;
+	while (index_r >= 0 && (len1 > 0 || len2 > 0 || carry > 0))
 	{
-		n1 = -n;
-		_putchar('_');
-	}
-	else
-	{
-		n1 = n;
-		_putchar('_');
-		if (n1 / 10)
+		int digit1 = 0, digit2 = 0;
+
+		if (len1 > 0)
 		{
-			print_number(n1 / 10);
+			digit1 = n1[len1 - 1] - '0';
+			len1--;
 		}
-		_putchar((n1 % 10) + '0');
+		if (len2 > 0)
+		{
+			digit2 = n2[len2 - 1] - '0';
+			len2--;
+		}
+		sum = digit1 + digit2 + carry;
+		carry = sum / 10;
+		r[index_r] = (sum % 10) + '0';
+		index_r--;
 	}
+	if (index_r < 0 && (len1 > 0 || len2 > 0 || carry > 0))
+		return (0);
+	return (r + index_r + 1);
 }
+
