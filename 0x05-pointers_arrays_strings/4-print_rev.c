ea659cc6a336b00b5d420543967307cc2e6ccd73
JeffMayi <geofrymayi@gmail.com>
2023-09-26 03:02:51 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,40 @@
 #include "main.h"
+
 /**
- * print_rev - prints a string, in reverse, followed by a new line
- * @s: string
- * Return: 0
+ * print_rev - function that prints a string
+ * in reverse followed by \n
+ * @s: character to test
  */
+
 void print_rev(char *s)
 {
-	int longi = 0;
-	int o;
+	/**Declarations*/
+	int len;
+	int start;
+	int end;
+	char *revarr;
 
-	while (*s != '\0')
+	if (!s)
 	{
-		longi++;
-		s++;
+	return;
 	}
-	s--;
-	for (o = longi; o > 0; o--)
+
+	/**initlization*/
+	len = strlen(s);
+	start = 0;
+	end = len - 1;
+	revarr = (char *)malloc((len + 1) * sizeof(char));
+	strcpy(revarr, s);
+	while (start < end)
 	{
-		putchar(*s);
+		/* swap the each end of string with each of the begging*/
+		char temp = revarr[start];
+
+		revarr[start] = revarr[end];
+		revarr[end] = temp;
+		start++;
+		end--;
 	}
-	putchar('\n');
+	puts(revarr);
+	free(revarr);
 }
