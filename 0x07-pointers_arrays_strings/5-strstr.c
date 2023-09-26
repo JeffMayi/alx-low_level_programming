35f210729cf19479ee6f89c1dbb0fd41b67b57d6
JeffMayi <geofrymayi@gmail.com>
2023-09-25 23:56:45 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints a numbers from 0-14
+ * followed by a new line.
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (j < 10)
+	{
+		while (i < 15)
+		{
+			if (i / 10 > 0)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+			i++;
+		}
+		_putchar('\n');
+		i = 0;
+		j++;
+	}
+
+}
