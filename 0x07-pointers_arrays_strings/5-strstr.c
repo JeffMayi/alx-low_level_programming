9c07fad38c3c83c381f8c64a1865f6824d11ff98
JeffMayi <geofrymayi@gmail.com>
2023-09-26 00:28:32 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,33 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * more_numbers - function that prints a numbers from 0-14
- * followed by a new line.
+ * *_strstr - function that locates a substring.
+ * @haystack: the string
+ * @needle: substring to match (remember all of it)
+ * Return: pointer to the beginning of the located needle in haystack
  */
 
-void more_numbers(void)
+char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
-	int j = 0;
-
-	while (j < 10)
+	/* loop throw the first string*/
+	while (haystack[i] != '\0')
 	{
-		while (i < 15)
-		{
-			if (i / 10 > 0)
-				_putchar(i / 10 + '0');
+		/*alway start second string from begining*/
+		/*first string we want to modify*/
+		/*(move to check every character) so we copy it*/
+		char *ptr0 = &haystack[i];
+		char *ptr1 = needle;
 
-			_putchar(i % 10 + '0');
-			i++;
+		while (*ptr0 == *ptr1 && *ptr1 != '\0')
+		{
+			ptr0++;
+			ptr1++;
 		}
-		_putchar('\n');
-		i = 0;
-		j++;
+		/*that mean we made it :) */
+		if (*ptr1 == '\0')
+			return (&haystack[i]);
+		i++;
 	}
 
+	return (NULL);
 }
